6ca2a63da20b79a942b7e19e8226ad0fd9a31c09
Daler <alyamov.daler.secure@gmail.com>
2021-01-19 11:21:36 +0500

    Удалены лишние файлы

---
+++ src/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import DropdownPage from './pages/DropdownPage'
+import data from './data.json'
+import {
+  BrowserRouter as Router, 
+  Link, 
+  Switch, 
+  Route
+} from 'react-router-dom'
+
+const App = () => {
+  return(
+    <div className="container">
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/DropdownPage/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DropdownPage = () => {
+  return (
+    <div className="dropdown-page"></div>
+  )
+}
+
+export default DropdownPage
