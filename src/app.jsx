97d5d47866e479c9bfaef47b7afcc20c1456fa13
Daler <alyamov.daler.secure@gmail.com>
2021-01-20 13:50:18 +0500

    очищено - 2

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import DropdownPage from 'pages/dropdown'
-import NotFound from 'pages/notFound'
-import data from './data.json'
-import Button from 'components/button'
+
 import {
   BrowserRouter as Router, 
   Link, 
