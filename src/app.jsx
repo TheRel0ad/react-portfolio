57fee1b709d33b481777d8962a9141e8f971606b
Daler <alyamov.daler.secure@gmail.com>
2021-01-20 13:50:52 +0500

    очищено-3

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,5 @@
 import React from 'react'
 
-import {
-  BrowserRouter as Router, 
-  Link, 
-  Switch, 
-  Route
-} from 'react-router-dom'
-
 const App = () => {
   return(
     <div className="app"></div>
